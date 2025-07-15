a8a84a70062aabdf383ba09407631b73841dce14
Kareem Hassan <karimgohary.gohary@gmail.com>
2025-07-15 21:24:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import flask
 from flask import render_template
+import pandas as pd
 
 app=flask.Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("Home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def call(station, date):
+    file = ("data/TG_STAID" + str(station).zfill(6) + ".txt")
+    df = pd.read_csv(file, skiprows= 20, parse_dates=['    DATE'])
+    temp = df.loc[df['    DATE'] == date]['   TG'].squeeze() /10
+    return {
+        "temperature":temp,
+        "date": date,
+        "station": station
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
